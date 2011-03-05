db8c7658f3b04e9c8ff8a03618e00df9b1e23d80
marklundeberg <nanite@gmail.com>
2011-03-05 03:47:09 -0800

    Edited conf/metadata.php via GitHub

--- conf/metadata.php
+++ conf/metadata.php
@@ -5,6 +5,7 @@ $meta['dvips_path'] = array('string');
 $meta['convert_path'] = array('string');
 $meta['identify_path'] = array('string');
 $meta['tmp_dir'] = array('string');
+$meta['keep_tmp'] = array('onoff');
 $meta['image_format'] = array('string');
 $meta['colour'] = array('string');
 $meta['density'] = array('numeric');
