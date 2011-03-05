7dc1032f02dee91b0fe248b41e48d73155418bcb
marklundeberg <nanite@gmail.com>
2011-03-05 04:11:07 -0800

    Edited lang/en/settings.php via GitHub

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['latex_path'] = 'Path to <code>LaTeX</code> binary';
+$lang['latex_path'] = 'Path to <code>latex</code> binary';
 $lang['dvips_path'] = 'Path to <code>dvips</code> binary';
 $lang['convert_path'] = 'Path to ImageMagick <code>convert</code> binary';
 $lang['identify_path'] = 'Path to ImageMagick <code>identify</code> binary';
 $lang['image_format'] = 'Image format of rendered LaTeX: <code>png</code> supports transparency, but <code>jpg</code> is compact.';
 $lang['tmp_dir'] = 'Location of temporary folder';
-$lang['colour'] = 'Equation colour, written as {model}{value1,value2,...}. <a href="http://en.wikibooks.org/wiki/LaTeX/Colors" target="_blank" class="urlextern">Info on colour models here</a>. You should delete the image cache for this to take effect.';
-$lang['density'] = 'Rasterization density -- controls size of rendered LaTeX. You should delete the image cache for this to take effect.';
+$lang['colour'] = 'Equation colour, written as {model}{value1,value2,...}. <a href="http://en.wikibooks.org/wiki/LaTeX/Colors" target="_blank" class="urlextern">Info on colour models here</a>. You should delete the image cache for this change to affect old equations.';
+$lang['density'] = 'Rasterization density -- controls size of rendered LaTeX. You should delete the image cache for this change to affect old equations.';
 $lang['keep_tmp'] = 'Keep temporary tex/aux/log/dvi/ps files rather than deleting them after rendering. Turn on this option to debug your LaTeX rendering problems.';
