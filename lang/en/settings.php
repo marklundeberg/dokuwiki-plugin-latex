37fe2eebd9c481108f91c55ee287f16ec3188e07
marklundeberg <nanite@gmail.com>
2011-03-05 01:17:30 -0800

    Edited lang/en/settings.php via GitHub

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,10 +1,10 @@
 <?php
 
-$lang['latex_path'] = 'Path to LaTeX binary.';
-$lang['dvips_path'] = 'Path to dvips binary.';
-$lang['convert_path'] = 'Path to ImageMagick convert binary.';
-$lang['identify_path'] = 'Path to ImageMagick identify binary.';
-$lang['image_format'] = 'Image format of rendered LaTeX.';
-$lang['tmp_dir'] = 'Location of //temporary folder//.';
-$lang['colour'] = 'Equation colour as {model}{value1,value2,...}. <a href="http://en.wikibooks.org/wiki/LaTeX/Colors" target="_blank" class="urlextern">Info on colour models here</a>. You should delete the image cache for this to take effect.';
+$lang['latex_path'] = 'Path to LaTeX binary';
+$lang['dvips_path'] = 'Path to dvips binary';
+$lang['convert_path'] = 'Path to ImageMagick convert binary';
+$lang['identify_path'] = 'Path to ImageMagick identify binary';
+$lang['image_format'] = 'Image format of rendered LaTeX';
+$lang['tmp_dir'] = 'Location of temporary folder';
+$lang['colour'] = 'Equation colour written as {model}{value1,value2,...}. <a href="http://en.wikibooks.org/wiki/LaTeX/Colors" target="_blank" class="urlextern">Info on colour models here</a>. You should delete the image cache for this to take effect.';
 $lang['density'] = 'Rasterization density -- controls size of rendered LaTeX. You should delete the image cache for this to take effect.';
