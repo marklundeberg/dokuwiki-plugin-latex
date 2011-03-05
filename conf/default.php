4cc316132449e168b02a6decc8c1df843e54a045
marklundeberg <nanite@gmail.com>
2011-03-05 03:48:07 -0800

    Edited conf/default.php via GitHub

--- conf/default.php
+++ conf/default.php
@@ -6,6 +6,6 @@ $conf['convert_path'] = 'convert';
 $conf['identify_path'] = 'identify';
 $conf['image_format'] = 'png';
 $conf['tmp_dir'] = '/tmp';
-$conf['keep_tmp'] = true;
+$conf['keep_tmp'] = false;
 $conf['colour'] = '{rgb}{0.408,0.094,0.059}';
 $conf['density'] = 120;
