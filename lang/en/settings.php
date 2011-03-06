1a175cab59e33a27e43786686fe28cb9bbd31c06
Mark Lundeberg <nanite@gmail.com>
2011-03-06 00:39:03 -0800

    * added admin page for cache wiping/cleaning. 	* binary paths now include options, as applicable. 	* added config options for preamble/postamble. 	* added config options for max formula sizes 	* added a minimal ODT render mode. 	* added informative tooltip text on images of failed renders.
    Signed-off-by: Mark Lundeberg <nanite@gmail.com>

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -4,20 +4,20 @@ $lang['adminlink'] = 'You should delete the image cache from the <a href="'.
   wl($ID).'?do=admin&page=latex'.
   '" target="_blank" class=""wikilink1">admin page</a> for this change to affect old equations.';
 
-$lang['latex_path'] = 'Path to <code>latex</code> binary and options.';
-$lang['dvips_path'] = 'Path to <code>dvips</code> binary and options.';
-$lang['convert_path'] = 'Path to ImageMagick <code>convert</code> binary and options.
+$lang['latex_path'] = 'Path to <code>latex</code> program and options';
+$lang['dvips_path'] = 'Path to <code>dvips</code> program and options';
+$lang['convert_path'] = 'Path to ImageMagick <code>convert</code> program and options;
    <code>-density &lt;number&gt;</code> controls rendering size, and <code>-transparent &lt;colour&gt;</code>
    converts colour to transparency. '.$lang['adminlink'];
-$lang['identify_path'] = 'Path to ImageMagick <code>identify</code> binary.';
+$lang['identify_path'] = 'Path to ImageMagick <code>identify</code> program';
 $lang['xsize_limit'] = 'Maximum width (in pixels) of rendered LaTeX images';
 $lang['ysize_limit'] = 'Maximum height (in pixels) of rendered LaTeX images';
-$lang['string_length_limit'] = 'Maximum length of LaTeX code';
-$lang['image_format'] = 'Image format of rendered LaTeX: <code>png</code> looks best, but <code>gif</code> is more compact.';
+$lang['string_length_limit'] = 'Maximum length (in characters) of LaTeX code (not including pre/post-amble)';
+$lang['image_format'] = 'Image format of rendered LaTeX; <code>png</code> looks best, but <code>gif</code> is more compact.';
 $lang['tmp_dir'] = 'Location of temporary folder';
-$lang['keep_tmp'] = 'Keep temporary tex/aux/log/dvi/ps files, rather than deleting them after rendering? Turn on this option to debug your LaTeX rendering problems.';
+$lang['keep_tmp'] = 'Keep temporary tex/aux/log/dvi/ps files? Turn on this option to debug your LaTeX rendering problems.';
 
-$lang['preamble'] = 'LaTeX compilation preamble.
-Include <a href="http://en.wikibooks.org/wiki/LaTeX/Colors#Color_Models" target="_blank" class="urlextern">colours</a>,
+$lang['preamble'] = 'LaTeX compilation preamble;
+here you can include <a href="http://en.wikibooks.org/wiki/LaTeX/Colors#Color_Models" target="_blank" class="urlextern">colours</a>,
 new packages, and more. '.$lang['adminlink'];
-$lang['postamble'] = 'LaTeX compilation postamble.';
+$lang['postamble'] = 'LaTeX compilation postamble';
