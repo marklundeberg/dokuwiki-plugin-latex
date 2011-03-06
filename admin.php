a38d58a99412a432b8e339b078335efee77bb978
Mark Lundeberg <nanite@gmail.com>
2011-03-05 20:09:49 -0800

    Signed-off-by: Mark Lundeberg <nanite@gmail.com>

--- admin.php
+++ admin.php
@@ -57,16 +57,18 @@ class admin_plugin_latex extends DokuWiki_Admin_Plugin {
      * handle user request
      */
     function handle() {
-	  global $conf;
+	  global $conf, $config_cascade;
 	  $this->output = "";
-	  if(!isset($_POST['latexpurge']))
-		$this->output = "Yams.";
 	  if(isset($_POST['latexpurge']))
 	  {
+		$current_dir = getcwd();
 		$this->output .= "Want to purge:<br/><pre>";
-		foreach(glob($conf['mediadir'].'/latex/img*') as $fname)
+		foreach(glob($conf['mediadir'].'/latex/img*') as $fname) {
+			$base = basename($fname);
 			$this->output .= $fname."\n";
+		}
 		$this->output .= "</pre>";
+		touch($config_cascade['main']['local']); // touch config settings to force re-rendering.
 	  }
     }
  
@@ -79,7 +81,12 @@ class admin_plugin_latex extends DokuWiki_Admin_Plugin {
       ptln('<p>'.$this->output.'</p>');
       
       ptln('<form action="'.wl($ID).'?do=admin&page='.$this->getPluginName().'" method="post">');
-	        
+	  echo $this->getLang('btn_latexpurge');
+	  foreach(array('mtime','atime') as $which) {
+		echo '<LABEL><INPUT type="radio" name="purgetime" value="'.$which.'"/>';
+		echo $this->getLang('label_times')[$which];
+		echo '</LABEL>\n';
+	  }
 	  ptln('  <input type="submit" class="button" name="latexpurge"  value="'.$this->getLang('btn_latexpurge').'" />');
       ptln('</form>');
     }
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,6 +3,11 @@
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
 
-$lang['menu'] = 'LaTeX plugin tasks (remove cache)'; 
+$lang['menu'] = 'LaTeX plugin tasks (remove image cache)'; 
 
-$lang['btn_latexpurge'] = 'Purge LaTeX cache';
+$lang['label_purge'] = "Purge LaTeX cache files by\n";
+$lang['label_times'] = array('mtime'=>'modification time', 'atime'=>'access time');
+$lang['label_days'] = 'ALL';
+$lang['label_olderthan'] = 'older than';
+$lang['label_days'] = 'days';
+$lang['btn_purge'] = 'Go';
