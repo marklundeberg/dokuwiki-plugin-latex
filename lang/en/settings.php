00bea3fedce457466611946ba3ce345b92562fa8
Mark Lundeberg <nanite@gmail.com>
2011-03-09 21:01:07 -0800

    Signed-off-by: Mark Lundeberg <nanite@gmail.com>

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -13,7 +13,7 @@ $lang['identify_path'] = 'Path to ImageMagick <code>identify</code> program';
 $lang['xsize_limit'] = 'Maximum width (in pixels) of rendered LaTeX images';
 $lang['ysize_limit'] = 'Maximum height (in pixels) of rendered LaTeX images';
 $lang['string_length_limit'] = 'Maximum length (in characters) of LaTeX code (not including pre/post-amble)';
-$lang['image_format'] = 'Image format of rendered LaTeX; <code>png</code> looks best, but <code>gif</code> is more compact.';
+$lang['image_format'] = 'Image format of rendered LaTeX; <code>png</code> looks best, but <code>gif</code> is more compact, especially if you add <code>+antialias</code> to the <code>convert</code> options below.';
 $lang['tmp_dir'] = 'Location of temporary folder';
 $lang['keep_tmp'] = 'Keep temporary tex/aux/log/dvi/ps files? Turn on this option to debug your LaTeX rendering problems.';
 
