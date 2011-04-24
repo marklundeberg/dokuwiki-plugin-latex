6a8ebc7f4e38a432a7514de7e140dfd061d3774e
Mark Lundeberg <nanite@gmail.com>
2011-04-24 00:48:16 -0700

    :wiki:latex:tmp is now the default tmp directory.
    Signed-off-by: Mark Lundeberg <nanite@gmail.com>

--- conf/default.php
+++ conf/default.php
@@ -5,8 +5,8 @@ $conf['dvips_path'] = '/usr/bin/dvips -E';
 $conf['convert_path'] = '/usr/bin/convert -density 120 -trim -transparent "#FFFFFF"';
 $conf['identify_path'] = '/usr/bin/identify';
 $conf['image_format'] = 'png';
-$conf['tmp_dir'] = '/tmp';
-//$conf['tmp_dir'] = ($GLOBALS['conf'])['mediadir'].'/wiki/latex/tmp';
+//$conf['tmp_dir'] = '/tmp';
+$conf['tmp_dir'] = $GLOBALS['conf']['mediadir'].'/wiki/latex/tmp';
 $conf['keep_tmp'] = false;
 
 $conf['xsize_limit'] = 1000;
