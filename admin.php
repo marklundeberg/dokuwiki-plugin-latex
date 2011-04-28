70f829a1163bc386304bdb207eecfe78542f3ad5
Mark Lundeberg <nanite@gmail.com>
2011-04-28 00:56:18 -0700

    troubleshooter links should work now..
    Signed-off-by: Mark Lundeberg <nanite@gmail.com>

--- admin.php
+++ admin.php
@@ -241,14 +241,16 @@ class admin_plugin_latex extends DokuWiki_Admin_Plugin {
 			$data = array($testformula,DOKU_LEXER_UNMATCHED,'class'=>"latex_inline", 'title'=>"Math", NULL);
 			$this->doc = '';
 			$plug->render('xhtml', $this, $data);
-			$tmpw = $this->getConf('latex_namespace').'tmp:'.$plug->_latex->_tmp_filename;
+			$tmpw = $this->getConf('latex_namespace').':tmp:'.$plug->_latex->_tmp_filename;
 			$tmpf = $plug->_latex->_tmp_dir."/".$plug->_latex->_tmp_filename;
 			$tmpext = array('tex','log','aux','dvi','ps',$plug->_latex->_image_format);
 			foreach($tmpext as $ext) {
 				$fname = $tmpf.'.'.$ext;
-				if(is_file($fname))
-					ptln('<div class="success">File created: '.$this->render('{{'.$tmpw.'.'.$ext.'?linkonly}}').'</div>');
-				else
+				if(is_file($fname)) {
+					$rendstr = $this->render('{{'.$tmpw.'.'.$ext.'?linkonly|'.$fname.'}}');
+					$rendstr = preg_replace('/<\\/?p>/','',$rendstr);
+					ptln('<div class="success">File created: '.$rendstr.'</div>');
+				} else
 					ptln('<div class="error">File missing! '.$fname.'</div>');
 			}
 			if(! isset($_REQUEST['keep_tmp']))
